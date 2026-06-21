ae6a3af072ab69296b6ce0e7eb21176e43cb8d93
salah <salah1999jari@gmail.com>
2026-06-21 16:10:26 +0200

    push

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import insead from "../../Assets/Projects/insead.png";
 import intake from "../../Assets/Projects/intake.png";
+import AlMaimouni from "../../Assets/Projects/AlMaimouni.png";
 import boostfy from "../../Assets/Projects/boostify.png";
 import autoApply from "../../Assets/Projects/auto_apply.png";
 import globalExam from "../../Assets/Projects/global_exame.png";
@@ -37,8 +38,19 @@ function Projects() {
               isBlog={false}
               title="INTAKE Support"
               description="INSEAD IT Support Hub is a comprehensive rewrite of the campus assistance workflow. It introduces a seamless, location-aware experience for users seeking technical support, while providing technicians with real-time tools to manage assistance queues efficiently."
-              ghLink="https://github.com/salah127"
+              ghLink="https://github.com/youcisla/inseadassistance"
               demoLink="https://it-assistance-jj.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={AlMaimouni}
+              isBlog={false}
+              title="Al MAIMOUNI Restaurant"
+              description="Al MAIMOUNI Restaurant is a fine dining establishment offering a diverse menu of gourmet dishes, focusing on high-quality ingredients and exceptional service to provide a memorable culinary experience."
+              ghLink="https://github.com/salah127/Al-Maimouni"
+              demoLink="https://al-maimouni.vercel.app/"
             />
           </Col>
 
@@ -48,7 +60,7 @@ function Projects() {
               isBlog={false}
               title="Boostfy"
               description="Marketing/automation tool designed to enhance online growth and engagement, typically focusing on traffic optimization, performance boosting, and streamlining digital promotion processes across platforms."
-              ghLink="https://github.com/salah127"
+              ghLink="https://github.com/salah127/boostfy"
             />
           </Col>
 
@@ -58,7 +70,7 @@ function Projects() {
               isBlog={false}
               title="Auto-Apply"
               description="Automation tool that streamlines the job search process by automatically finding relevant listings and submitting applications across multiple platforms, helping users save time and increase application efficiency."
-              ghLink="https://github.com/salah127"
+              ghLink="https://github.com/salah127/Auto-Apply"
             />
           </Col>
 
@@ -68,7 +80,7 @@ function Projects() {
               isBlog={false}
               title="GlobalExam-Solver"
               description="Tool that automates solving exercises and test questions from the GlobalExam platform, helping users quickly generate answers and improve efficiency in language exam practice."
-              ghLink="https://github.com/salah127"
+              ghLink="https://github.com/salah127/GlobalExam-Solver"
             />
           </Col>
         </Row>
